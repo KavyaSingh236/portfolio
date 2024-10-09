d29e4fe272e1a058c7617b978a38f8c609833f8f
Kavya Singh <136486328+KavyaSingh236@users.noreply.github.com>
2024-10-09 22:52:21 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -10,6 +10,7 @@ export const skillsData = [
   'Bootstrap',
   'Node',  // Add this since MERN includes Node.js
   'Express',  // Add this to emphasize full MERN stack
+   'GSAP',
 
   // Additional Front-end Frameworks & Tools
   'Angular',
@@ -24,12 +25,12 @@ export const skillsData = [
   'C',
   'Python',
   'Java',
-  'R'
+  'R',
+  //Cloud 
+  'AWS',
+ 
 ];
 
-
-// Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
 
 
 // AVAILABLE SKILLS
