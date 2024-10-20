b8041dfc0e8fc63c133761604b3af74f9329537c
Kavya Singh <136486328+KavyaSingh236@users.noreply.github.com>
2024-10-20 22:56:08 +0530

    Update projectData.js

--- utils/data/projectData.js
+++ utils/data/projectData.js
@@ -19,21 +19,21 @@ const projectData = [
       description: "A  streamlined solution that simplifies organ donation by automating donor registration, coordinating organ transpor. Built with Flask, MySQL, and an intuitive interface, it enhances transparency, improves efficiency, and ensures better outcomes for patients in need of transplants.",
       githubLink: "https://github.com/KavyaSingh236/OrganDonation/tree/main",
       youtubeLink:"https://www.youtube.com/watch?v=MAR3K76W2N0", 
-      image: "https://media.licdn.com/dms/image/sync/v2/D5627AQFtx0abm9EWUA/articleshare-shrink_800/articleshare-shrink_800/0/1721987578534?e=1729098000&v=beta&t=TyQfSKEcHD5wqH0qyjlYkqJdiAwaspPwVq9Fu2dykBM"
+      image: "/projects/organ.png"
     },
     {
       title: "Tempsat",
       description: "A user-friendly weather platform offering accurate forecasts and insights, built with HTML, CSS, and JavaScript to help users make informed decisions.",
       githubLink: "https://github.com/KavyaSingh236/Tempsat",
       liveDemoLink: "https://kavyasingh236.github.io/Tempsat/", 
-      image: "https://media.licdn.com/dms/image/v2/D562DAQHs9FICrVLjnA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1716293832369?e=1729098000&v=beta&t=64eVfyfW5SdoOoky9bD8OY3UrCfFf1m7MxaNU9gETXo"
+      image: "/projects/weather.png"
     },
     {
       title: "Pixel Craft",
       description: "An easy-to-use online image editor focused on simplicity, allowing users to crop, resize, and edit images efficiently, making it ideal for quick image manipulation.",
       githubLink: "https://github.com/KavyaSingh236/Pixel",
       liveDemoLink: "https://kavyasingh236.github.io/Pixel/", 
-      image: "https://media.licdn.com/dms/image/v2/D562DAQECDnFgMCDx8Q/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1726940777998?e=1729098000&v=beta&t=bo1f253XoXXUrZr5G2cZ7enHFrnO7fEB9IEGVsWTt3A"
+      image: "/projects/pixel.png"
     }
   ];
   
