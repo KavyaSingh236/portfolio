f9405233d72d1d30e2b0f24735e5e593bd27a35f
Kavya Singh <136486328+KavyaSingh236@users.noreply.github.com>
2024-10-23 19:04:45 +0530

    Update certificate.js

--- utils/data/certificate.js
+++ utils/data/certificate.js
@@ -7,30 +7,37 @@ export const academicCertificates = [
     },
     {
       id: 2,
+      title: "Master Course of Edge Computing 3.0",
+      image: "/certificate/acadcertificatenew.png",
+      link: "https://media.licdn.com/dms/image/v2/D562DAQFGSDRlLyp7eA/profile-treasury-image-shrink_1920_1920/profile-treasury-image-shrink_1920_1920/0/1729690257379?e=1730296800&v=beta&t=onJYj1PYnWOGmXMBkP472aGjUcFcfaJvR5lTKlt0Q7I"
+    },
+    
+    {
+      id: 3,
       title: "JITHACK'24 - Jyothy Institute of Technology",
       image: "/certificate/acadcertificate2.png",
       link: "https://media.licdn.com/dms/image/v2/D562DAQFimiAZbp40RA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1719765600210?e=1727881200&v=beta&t=SKeUG4UTNftd3ZidUmthBnhCv59-xtN7DOK9BX_D9xA"
     },
     {
-      id: 3,
+      id: 4,
       title: "International Level Student Workshop - 2k24 on Data Science using Python",
       image: "/certificate/acadcertificate3.png",
       link: "https://media.licdn.com/dms/image/v2/D562DAQG9VFn6KMXBTg/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1719385245366?e=1727881200&v=beta&t=zb8vaMX0aPPp_ZYW6XN7jzrDY81iLyPRqxSTmCKif3A"
     },
     {
-      id: 4,
+      id: 5,
       title: "Certificate of Appreciation for participating in the event 'Code Fiesta'",
       image: "/certificate/acadcertificate4.png",
       link: "https://media.licdn.com/dms/image/v2/D562DAQEJEPJ9li61eA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1719381497481?e=1727888400&v=beta&t=JByTGZdICwg2k_EPW3n59i2h6xNKcahxpopf6rji1Uc"
     },
     {
-      id: 5,
+      id: 6,
       title: "Internship Program on Object Oriented Programming with Java",
       image: "/certificate/acadcertificate5.png",
       link: "https://media.licdn.com/dms/image/v2/D562DAQFXITNU4PxRqg/profile-treasury-image-shrink_1280_1280/profile-treasury-image-shrink_1280_1280/0/1719368446216?e=1727888400&v=beta&t=vc50pL7ifqOm2Z_6IRZbJmczXIVpkXww3W6SSDACYq0"
     },
     {
-      id: 6,
+      id: 7,
       title: "Aptitude & Soft Skills Training",
       image: "/certificate/acadcertificate6.png",
       link: "https://media.licdn.com/dms/image/v2/D562DAQGCJ2coMZOOvA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1719386934336?e=1727888400&v=beta&t=-vBdmzIOo_91XP2sghvwX1jrr0YTurf5vXw1LFhCwBo"
