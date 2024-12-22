002672b40a10aaacb62646f564b0e86a978ffd6b
Kavya Singh <136486328+KavyaSingh236@users.noreply.github.com>
2024-12-22 19:32:42 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,4 +8,5 @@ export const personalData = {
   linkedIn: 'https://www.linkedin.com/in/kavya-singh-690888292/',
   youtube:'https://www.youtube.com/@KavyaSingh-t2g',
   instagram: 'https://www.instagram.com/sweet_syntax/?igsh=MXhqcm94aTVlM3U0cw%3D%3D',
-  resume: "https://drive.google.com/file/d/1uSAh12EVU0EjlJ341KdHIbDJrC6NXUrX/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1uSAh12EVU0EjlJ341KdHIbDJrC6NXUrX/view"
+}
