ec2dc28a567b739294f39166b5a6e9ef102253b9
Kavya Singh <136486328+KavyaSingh236@users.noreply.github.com>
2025-09-30 00:27:51 -0500

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,14 +1,22 @@
 export const educations = [
   {
     id: 1,
+    title: "Masters in Data Science",
+    duration: "2025-2027",
+    institution: "University of North Texas",
+    image: "/education/education3.jpg" ,  
+    link:"https://www.unt.edu/"
+  },
+  {
+    id: 2,
     title: "Computer Science Engineering",
-    duration: "2021 - Present",
+    duration: "2021 - 2025",
     institution: "KS Institute of Technology",
     image: "/education/education1.png" ,  
     link:"https://www.ksit.ac.in/"
   },
   {
-    id: 2,
+    id: 3,
     title: "Pre-University Course (PCMCs)",
     duration: "2019 - 2021",
     institution: "PES PU COLLEGE",
